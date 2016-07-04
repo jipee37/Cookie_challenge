b9ef4ac4e75a4c6acad0323b16c360a9407f24d9
jipee37 <jipee37@yahoo.fr>
2016-07-04 23:33:02 +0200

    étape 10 - étape 7

--- cookie-dingler.js
+++ cookie-dingler.js
@@ -1,14 +1,30 @@
 var click = document.getElementById('bouton');
-
 var view = document.getElementById('affichage');
 var score = 0;
-var countClick = 1;
-  
+var compteur = 1;
+var multi = document.getElementById('multiplicateur');  
+var price = 50; /* pour réinitialiser le décompte*/
+
 
   var addScore = function(){
      if (score >= 0) {
-         score = score + countClick;
+         score = score + compteur;
          view.innerHTML = score;
          }
   }
   click.addEventListener("click", addScore);
+
+
+var incrementer = function(){
+         
+         if (score >= price) {
+         compteur = compteur * 2;
+         score = score - price;
+         price = price * 2;
+         multi.value = "multiplicateur: " + compteur + " next increment, price " + price;
+     }
+        else {
+          alert("Ne laisse pas tomber, je sais c'est pas si facile !");
+      }
+  }
+  multi.addEventListener("click", incrementer);
